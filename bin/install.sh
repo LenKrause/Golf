ea107fa277401f7608fb3af9ae1fd1601ef968d5
Len Krause <len@excellware.com>
2018-11-18 18:48:44 -0500

    Revised scripts

--- bin/install.sh
+++ bin/install.sh
@@ -9,5 +9,8 @@ echo Compiling programs...
 umask 005
 /u/basis/bbj/bin/bbjcpl -d/u/CDI/LK/pg6 ./$(dirname $0)/../src/* 2>/u/CDI/tmp/$$
 if [ -s /u/CDI/tmp/$$ ]; then
-  cat /u/CDI/tmp/$$ | mail -s "Golf compile errors" len@excellware.OM
+  cat /u/CDI/tmp/$$ | mail -s "Golf compile errors" len@excellware.com
+  echo Emailed list of compile errors
+else
+  echo No compile errors detected
 fi
