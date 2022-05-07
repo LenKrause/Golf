dfb5e7c2d2243812eb0f6d918f48e06932bfd726
Len Krause <len@excellware.com>
2022-05-07 11:13:50 -0400

    inlcude skin posting sheet
    phase out score posting sheet

--- bin/gitgolf.sh
+++ bin/gitgolf.sh
@@ -5,7 +5,7 @@ cd /u/CDI/LK/gitrepo/Golf
 git pull
 
 # compile code
-/u/basis/bbj/bin/bbjcpl -d/u/CDI/LK/bbj/ src/*.bbj src/GW011 src/GW111
+/u/basis/bbj/bin/bbjcpl -d/u/CDI/LK/bbj/ src/*.bbj src/GW011 src/GW045 src/GW111
 
 # copy css
 cp css/*.css /u/CDI/LK/web/dynaweb/
