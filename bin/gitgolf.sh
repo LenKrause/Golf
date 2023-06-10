a75d0abf4f19398bf49a07c6d59a650b9e39789d
Len Krause <len@excellware.com>
2023-06-09 22:36:24 -0400

    script

--- bin/gitgolf.sh
+++ bin/gitgolf.sh
@@ -1,16 +1,4 @@
 #!/bin/sh
-<<<<<<< HEAD
-## pull Golf from github and install bbj programs
-#
-#cd /u/CDI/LK/gitrepo/Golf
-#git pull
-#
-## compile code
-#/u/basis/bbj/bin/bbjcpl -d/u/CDI/LK/bbj/ src/*.bbj src/GW011 src/GW045 src/GW111
-#
-## copy css
-#cp css/*.css /u/CDI/LK/web/dynaweb/
-=======
 # pull Golf from github and install bbj programs
 
 cd /u/CDI/LK/gitrepo/Golf
@@ -21,4 +9,3 @@ git pull
 
 # copy css
 cp css/*.css /u/CDI/LK/web/dynaweb/
->>>>>>> f041460a61cd10329c55d3b25f45461e11f302f4
